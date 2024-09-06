f4eab18d1cf8ff283dc86561dc96720e892745e4
Daniel Ranner <daniel.ranner@jollydays.com>
2024-09-06 19:04:26 +0200

    Added a first, hacky TerminalUI view

--- src/main/java/ninja/ranner/conductor/ConductorApplication.java
+++ src/main/java/ninja/ranner/conductor/ConductorApplication.java
@@ -1,13 +1,60 @@
 package ninja.ranner.conductor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.shell.component.message.ShellMessageBuilder;
+import org.springframework.shell.component.view.TerminalUIBuilder;
+import org.springframework.shell.component.view.control.BoxView;
+import org.springframework.shell.component.view.event.KeyEvent;
+import org.springframework.shell.geom.HorizontalAlign;
+import org.springframework.shell.geom.VerticalAlign;
+
+import java.io.IOError;
 
 @SpringBootApplication
 public class ConductorApplication {
 
+    Logger logger = LoggerFactory.getLogger(ConductorApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(ConductorApplication.class, args);
+    }
+
+    @Bean
+    @ConditionalOnProperty(value = "spring.shell.interactive.enabled", havingValue = "true", matchIfMissing = true)
+    CommandLineRunner renderUi(TerminalUIBuilder terminalUIBuilder) {
+        return (String... args) -> {
+            var ui = terminalUIBuilder.build();
+
+            var boxView = new BoxView();
+            boxView.setShowBorder(true);
+            boxView.setDrawFunction((screen, rectangle) -> {
+                var writer = screen.writerBuilder().build();
+                writer.text("Hello, Ensemblers!", rectangle, HorizontalAlign.CENTER, VerticalAlign.CENTER);
+                return rectangle;
+            });
+            ui.configure(boxView);
+            ui.setRoot(boxView, true);
+
+            var eventLoop = ui.getEventLoop();
+            eventLoop.keyEvents().subscribe(keyEvent -> {
+                if (keyEvent.getPlainKey() == KeyEvent.Key.q) {
+                    eventLoop.dispatch(ShellMessageBuilder.ofInterrupt());
+                }
+            });
+
+            try {
+                ui.run();
+            } catch (IOError e) {
+                logger.info("Caught IOError: {}", e.getMessage());
+                // ignore
+            }
+        };
     }
 
 }
--- src/test/java/ninja/ranner/conductor/ConductorApplicationTests.java
+++ src/test/java/ninja/ranner/conductor/ConductorApplicationTests.java
@@ -3,9 +3,13 @@ package ninja.ranner.conductor;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @Tag("Spring")
+@TestPropertySource(properties = {
+        "spring.shell.interactive.enabled=false"
+})
 class ConductorApplicationTests {
 
     @Test
