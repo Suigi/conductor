f791129ff033dcbb3cb00f4e06775ca4a798688f
Daniel Ranner <daniel.ranner@jollydays.com>
2024-09-06 18:36:08 +0200

    Added test run configs

--- src/test/java/ninja/ranner/conductor/ConductorApplicationTests.java
+++ src/test/java/ninja/ranner/conductor/ConductorApplicationTests.java
@@ -1,9 +1,11 @@
 package ninja.ranner.conductor;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Tag("Spring")
 class ConductorApplicationTests {
 
     @Test
