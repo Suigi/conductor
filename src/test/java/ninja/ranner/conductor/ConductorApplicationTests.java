88e4d453e1ed4aaa9e54c5e83150cdc30d56b819
Lada Kesseler <lada.kesseler1@t-mobile.com>
2024-09-13 09:59:05 -0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/test/java/ninja/ranner/conductor/ConductorApplicationTests.java

--- src/test/java/ninja/ranner/conductor/ConductorApplicationTests.java
+++ src/test/java/ninja/ranner/conductor/ConductorApplicationTests.java
@@ -3,12 +3,16 @@ package ninja.ranner.conductor;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.shell.component.view.screen.DefaultScreen;
+import org.springframework.shell.component.view.screen.Screen;
+import org.springframework.shell.geom.Rectangle;
+import org.springframework.shell.test.ShellScreenAssert;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @Tag("Spring")
 @TestPropertySource(properties = {
-        "spring.shell.interactive.enabled=false"
+    "spring.shell.interactive.enabled=false"
 })
 class ConductorApplicationTests {
 
@@ -18,6 +22,9 @@ class ConductorApplicationTests {
 
     @Test
     void pants() {
-        // start here
+        Screen screen = new DefaultScreen(10, 80);
+        Rectangle result = ConductorApplication.something(screen, new Rectangle(0, 0, 10, 80));
+        System.out.println();
+
     }
 }
