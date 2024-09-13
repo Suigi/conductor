d2c156b8d3adf0be12f50494215723cfceada6c8
Daniel Ranner <daniel.ranner@jollydays.com>
2024-09-14 00:11:08 +0200

    Printer now prints remaining time to stdout.

--- src/main/java/ninja/ranner/conductor/adapter/out/terminal/Printer.java
+++ src/main/java/ninja/ranner/conductor/adapter/out/terminal/Printer.java
@@ -1,7 +1,14 @@
 package ninja.ranner.conductor.adapter.out.terminal;
 
+import ninja.ranner.conductor.domain.Timer;
+
 public class Printer {
-    public String print() {
-        return "";
+
+    public void print(Timer timer) {
+        if (timer.remainingSeconds() > 0) {
+            System.out.println("Time left: " + timer.remainingSeconds() + " second");
+        } else {
+            System.out.println("Turn is up!");
+        }
     }
 }
--- src/test/java/ninja/ranner/conductor/adapter/out/terminal/PrinterTest.java
+++ src/test/java/ninja/ranner/conductor/adapter/out/terminal/PrinterTest.java
@@ -1,27 +1,32 @@
 package ninja.ranner.conductor.adapter.out.terminal;
 
+import ninja.ranner.conductor.domain.Timer;
 import org.junit.jupiter.api.Test;
 
+import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PrinterTest {
+
     @Test
-    void withTimerOf0Seconds_printsEmptyString() {
+    void withTimerOf0Seconds_printsTurnIsUp() throws Exception {
         Printer printer = new Printer();
 
-        String output = printer.print();
+        String output = tapSystemOut(() ->
+                printer.print(new Timer(0)));
 
         assertThat(output)
-               .isEqualTo("");
+                .isEqualTo("Turn is up!\n");
     }
 
     @Test
-    void withTimerOf1Second_prints1() {
+    void withTimerOf1Second_prints1() throws Exception {
         Printer printer = new Printer();
 
-        String output = printer.print();
+        String output = tapSystemOut(() ->
+                printer.print(new Timer(1)));
 
         assertThat(output)
-                .isEqualTo("");
+                .isEqualTo("Time left: 1 second\n");
     }
 }
