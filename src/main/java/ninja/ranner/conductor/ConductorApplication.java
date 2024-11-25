50970cff9cebe96f255c58f406b68c0ab946bfa6
Daniel Ranner <daniel.ranner@jollydays.com>
2024-11-25 15:17:26 +0100

    Extracted field for base url

--- src/main/java/ninja/ranner/conductor/ConductorApplication.java
+++ src/main/java/ninja/ranner/conductor/ConductorApplication.java
@@ -4,11 +4,12 @@ import ninja.ranner.conductor.application.Root;
 
 public class ConductorApplication {
 
+    private static final String apiBaseUrl = "http://localhost:8088";
     private static final String timerName = "Ensemble Timer Demo";
 
     public static void main(String[] args) throws Exception {
         Root
-                .create("http://localhost:8088", timerName)
+                .create(apiBaseUrl, timerName)
                 .startInBackground()
                 .join();
     }
